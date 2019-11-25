bf83a75d3be700282f7cf3467f47d1067638ae3d
Glenn Oberlé <oberle.glenn@icloud.com>
2019-11-25 15:45:10 +0100

    transition js & css

--- js/app.js
+++ js/app.js
@@ -4,17 +4,17 @@ var boy = document.getElementsByTagName("img")[0];
 var titre1 = document.getElementsByTagName("h1")[0];
 var titre2 = document.getElementsByTagName("h1")[1];
 var fleche = document.getElementsByTagName("img")[1];
-
-function scrolled(){
-	var curScroll = document.documentElement.scrollTop || document.body.scrollTop,
+var menu = document.getElementsByTagName("ul")[0];
+var curScroll = document.documentElement.scrollTop || document.body.scrollTop,
 		windowHeight = document.documentElement.clientHeight;
-	
-	if (curScroll >= boy.offsetTop - windowHeight + boy.offsetHeight/2.5){
+        
+
+if (curScroll >= boy.offsetTop - windowHeight + boy.offsetHeight/2.5){
         boy.style.opacity = "1";
         titre1.style.opacity ="1";
         titre2.style.opacity ="1";
         fleche.style.opacity ="1";
-	}
+        menu.style.opacity = "1";
 }
 
 addEvent(window, "scroll", scrolled, false);
