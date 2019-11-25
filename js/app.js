ec839ee36f482cf6dd1a27af7a32ced6512bead2
Glenn Oberlé <oberle.glenn@icloud.com>
2019-11-25 15:24:52 +0100

    mise en place de js

---
+++ js/app.js
@@ -0,0 +1,20 @@
+function addEvent(a,b,c,d){d = !d && false;(a.addEventListener)?a.addEventListener(b,c,d):a.attachEvent('on'+b,c);}
+
+var boy = document.getElementsByTagName("img")[0];
+var titre1 = document.getElementsByTagName("h1")[0];
+var titre2 = document.getElementsByTagName("h1")[1];
+var fleche = document.getElementsByTagName("img")[1];
+
+function scrolled(){
+	var curScroll = document.documentElement.scrollTop || document.body.scrollTop,
+		windowHeight = document.documentElement.clientHeight;
+	
+	if (curScroll >= boy.offsetTop - windowHeight + boy.offsetHeight/2.5){
+        boy.style.opacity = "1";
+        titre1.style.opacity ="1";
+        titre2.style.opacity ="1";
+        fleche.style.opacity ="1";
+	}
+}
+
+addEvent(window, "scroll", scrolled, false);
