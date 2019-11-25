7e19d1c7f475cde450c7dac5b40240300aefe047
Glenn Oberlé <oberle.glenn@icloud.com>
2019-11-25 16:11:57 +0100

    php

---
+++ contact.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        <input type="submit" value="envoyer">
+    </form>
+</body>
+</html>
