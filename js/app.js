31c74eb02b2702978e24c3f5c5f7f737e01d0a28
Glenn Oberlé <oberle.glenn@icloud.com>
2019-11-29 12:31:44 +0100

    debut de la partie 'à propos :'

--- js/app.js
+++ js/app.js
@@ -1,14 +1,11 @@
-function addEvent(a,b,c,d){d = !d && false;(a.addEventListener)?a.addEventListener(b,c,d):a.attachEvent('on'+b,c);}
-
 var boy = document.getElementsByTagName("img")[0];
 var titre1 = document.getElementsByTagName("h1")[0];
 var titre2 = document.getElementsByTagName("h1")[1];
 var fleche = document.getElementsByTagName("img")[1];
 var menu = document.getElementsByTagName("ul")[0];
 var curScroll = document.documentElement.scrollTop || document.body.scrollTop,
-		windowHeight = document.documentElement.clientHeight;
-        
-
+                windowHeight = document.documentElement.clientHeight;
+                      
 if (curScroll >= menu.offsetTop - windowHeight + menu.offsetHeight/2.5){
         boy.style.opacity = "1";
         titre1.style.opacity ="1";
