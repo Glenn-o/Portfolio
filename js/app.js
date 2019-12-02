ab8df0f87b2be87a01f7bf5804e470adcaf49f91
Glenn Oberlé <oberle.glenn@icloud.com>
2019-12-02 12:45:59 +0100

    debut du responsive

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-var boy = document.getElementsByTagName("img")[0];
+var boy = document.getElementsByTagName("img")[1];
 var titre1 = document.getElementsByTagName("h1")[0];
 var titre2 = document.getElementsByTagName("h1")[1];
-var fleche = document.getElementsByTagName("img")[1];
+var fleche = document.getElementsByTagName("img")[2];
 var menu = document.getElementsByTagName("ul")[0];
 var curScroll = document.documentElement.scrollTop || document.body.scrollTop,
                 windowHeight = document.documentElement.clientHeight;
