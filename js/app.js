7c57f080baf7736ded013ea24afa25eeace240e0
Glenn Oberlé <oberle.glenn@icloud.com>
2019-12-05 16:56:20 +0100

    title

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-window.parent.document.title = 'Portfolio'
-
 var allowedKeys = {
     69: 'e',
     65: 'a',
@@ -8,29 +6,29 @@ var allowedKeys = {
     82: 'r',
     71: 'g'
   };
-  var eggCode = ['e', 'a', 's', 't', 'e', 'r', 'e', 'g', 'g'];
-  var eggCodePosition = 0;
-  var egg = document.querySelector('.egg');
+var eggCode = ['e', 'a', 's', 't', 'e', 'r', 'e', 'g', 'g'];
+var eggCodePosition = 0;
+var egg = document.querySelector('.egg');
   
-  document.addEventListener('keydown', function(e) {
+document.addEventListener('keydown', function(e) {
 
-    var key = allowedKeys[e.keyCode];
+  var key = allowedKeys[e.keyCode];
 
-    var requiredKey = eggCode[eggCodePosition];
-  
-    if (key == requiredKey) {
-  
-      eggCodePosition++;
-  
-      if (eggCodePosition == eggCode.length) {
-        activate();
-        eggCodePosition = 0;
-      }
-    } else {
+  var requiredKey = eggCode[eggCodePosition];
+
+  if (key == requiredKey) {
+
+    eggCodePosition++;
+
+    if (eggCodePosition == eggCode.length) {
+      activate();
       eggCodePosition = 0;
     }
-  });
+  } else {
+    eggCodePosition = 0;
+  }
+});
 
 function activate() {
-    egg.style.display = "flex"
+  egg.style.display = "flex"
 }
