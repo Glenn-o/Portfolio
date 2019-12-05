6860cbf17f0b8afb048d8963ca3ded3cb3dc2539
Glenn Oberlé <oberle.glenn@icloud.com>
2019-12-05 12:39:47 +0100

    page definitive + easter egg

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
 window.parent.document.title = 'Portfolio'
+
+var allowedKeys = {
+    69: 'e',
+    65: 'a',
+    83: 's',
+    84: 't',
+    82: 'r',
+    71: 'g'
+  };
+  var eggCode = ['e', 'a', 's', 't', 'e', 'r', 'e', 'g', 'g'];
+  var eggCodePosition = 0;
+  var egg = document.querySelector('.egg');
+  
+  document.addEventListener('keydown', function(e) {
+
+    var key = allowedKeys[e.keyCode];
+
+    var requiredKey = eggCode[eggCodePosition];
+  
+    if (key == requiredKey) {
+  
+      eggCodePosition++;
+  
+      if (eggCodePosition == eggCode.length) {
+        activate();
+        eggCodePosition = 0;
+      }
+    } else {
+      eggCodePosition = 0;
+    }
+  });
+
+function activate() {
+    egg.style.display = "flex"
+}
