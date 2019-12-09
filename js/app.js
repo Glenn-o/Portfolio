59d8714251f4a354cea6b39a84952a7f1bd789fd
Glenn Oberlé <oberle.glenn@icloud.com>
2019-12-09 16:43:50 +0100

    modification

--- js/app.js
+++ js/app.js
@@ -1,36 +1,49 @@
-var allowedKeys = {
-    69: 'e',
-    65: 'a',
-    83: 's',
-    84: 't',
-    82: 'r',
-    71: 'g',
-    32: 'space'
-  };
-var eggCode = ['e', 'a', 's', 't', 'e', 'r', 'space', 'e', 'g', 'g'];
-var eggCodePosition = 0;
-var egg = document.querySelector('.egg');
+jQuery(document).ready(function($){
+	var contentSections = $('.cd-section'),
+		navigationItems = $('#cd-vertical-nav a');
+
+	updateNavigation();
+	$(window).on('scroll', function(){
+		updateNavigation();
+	});
+
+	//smooth scroll to the section
+	navigationItems.on('click', function(event){
+        event.preventDefault();
+        smoothScroll($(this.hash));
+    });
+    //smooth scroll to second section
+    $('.cd-scroll-down').on('click', function(event){
+        event.preventDefault();
+        smoothScroll($(this.hash));
+    });
+
+    //open-close navigation on touch devices
+    $('.touch .cd-nav-trigger').on('click', function(){
+    	$('.touch #cd-vertical-nav').toggleClass('open');
   
-document.addEventListener('keydown', function(e) {
+    });
+    //close navigation on touch devices when selectin an elemnt from the list
+    $('.touch #cd-vertical-nav a').on('click', function(){
+    	$('.touch #cd-vertical-nav').removeClass('open');
+    });
 
-  var key = allowedKeys[e.keyCode];
+	function updateNavigation() {
+		contentSections.each(function(){
+			$this = $(this);
+			var activeSection = $('#cd-vertical-nav a[href="#'+$this.attr('id')+'"]').data('number') - 1;
+			if ( ( $this.offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/2 > $(window).scrollTop() ) ) {
+				navigationItems.eq(activeSection).addClass('is-selected');
+			}else {
+				navigationItems.eq(activeSection).removeClass('is-selected');
+			}
+		});
+	}
 
-  var requiredKey = eggCode[eggCodePosition];
-
-  if (key == requiredKey) {
-
-    eggCodePosition++;
-
-    if (eggCodePosition == eggCode.length) {
-      activate();
-      window.scroll(0, 0);
-      eggCodePosition = 0;
-    }
-  } else {
-    eggCodePosition = 0;
-  }
+	function smoothScroll(target) {
+        $('body,html').animate(
+        	{'scrollTop':target.offset().top},
+        	600
+        );
+	}
 });
-
-function activate() {
-  egg.style.display = "flex"
-}
